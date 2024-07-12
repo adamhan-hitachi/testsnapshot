4e6fb1238a17484a6e04f9c2b302af2bd2b58e8c
adam.han <adam.han@hitachivantara.com>
2024-07-12 13:32:19 +0100

    test regular file in snapshot

--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@ const std::string fs_path = sub_volume_path + "5763592a-833b-4250-9c31-5e6e23b55
 const std::string snap_dir = fs_path + "/.snap";
 
 const std::string dir_name{"test-snapshot-dir"};
+const std::string file_name{"test-snapshot-file"};
 const std::string xattr_name{"user.test-snapshot-xattr"};
 const std::string xattr_value{"test-snapshot-xattr-value"};
 const std::string snap_name{"test-snapshot-snap"};
@@ -236,6 +237,37 @@ int main(int, char**){
         return result;
     }
 
+    struct ceph_statx file_sb;
+    Fh* fhp_test_file = nullptr;
+    Inode* test_file_inode = nullptr;
+
+    result = ceph_ll_create(mount.get(), inode_fs, file_name.c_str(), 0755, O_CREAT, &test_file_inode, &fhp_test_file, &file_sb, CEPH_STATX_ALL_STATS, 0, user_perms.get());
+    if (result) {
+        std::cerr << "Failed to create file " << file_name << ": error " << -result << " (" << ::strerror(-result) << ")" << std::endl;
+        return result;
+    }
+
+    result = ceph_ll_close(mount.get(), fhp_test_file);
+    if (result) {
+      std::cerr << "Failed to close file " << file_name <<  ": error " << -result << " (" << ::strerror(-result) << ")" << std::endl;
+    }
+
+    std::shared_ptr<Inode> scoped_test_file_inode(test_file_inode, [mount](Inode* inode) {
+        ceph_ll_put(mount.get(), inode);
+    });
+
+    result = ceph_ll_setxattr(mount.get(), test_file_inode, xattr_name.c_str(), xattr_value.c_str(), xattr_value.size(), 0, user_perms.get());
+    if (result) {
+        std::cerr << "Failed to set xattr " << xattr_name << ": error " << -result << " (" << ::strerror(-result) << ")" << std::endl;
+        return result;
+    }
+
+    result = ceph_ll_getxattr(mount.get(), test_file_inode, xattr_name.c_str(), nullptr, 0, user_perms.get());
+    if (result < 0) {
+        std::cerr << "Failed to get length of active dir's xattr " << xattr_name << ": error " << -result << " (" << ::strerror(-result) << ")" << std::endl;
+        return result;
+    }
+
   #if 1
     std::string create_snap_cmd = "ceph fs subvolume snapshot create " + volume + " " + sub_volume + " " + snap_name;
     std::cout << create_snap_cmd << std::endl;
@@ -290,12 +322,13 @@ int main(int, char**){
     }
 
     const uint64_t snap_id = snap_info.id;
-    const vinodeno vivo = {dir_sb.stx_ino, snap_id};
+    const vinodeno vivo_dir = {dir_sb.stx_ino, snap_id};
+    const vinodeno vivo_file = {file_sb.stx_ino, snap_id};
 
     Inode* test_dir_inode_snap = nullptr;
-    result = ceph_ll_lookup_vino(mount.get(), vivo, &test_dir_inode_snap);
+    result = ceph_ll_lookup_vino(mount.get(), vivo_dir, &test_dir_inode_snap);
     if (result) {
-        std::cerr << "Failed to lookup inode of directory {" << std::to_string(vivo.ino.val) << ", " << std::to_string(vivo.snapid.val) << "} in snapshot: error " << -result << " (" << ::strerror(-result) << ")" << std::endl;
+        std::cerr << "Failed to lookup inode of directory {" << std::to_string(vivo_dir.ino.val) << ", " << std::to_string(vivo_dir.snapid.val) << "} in snapshot: error " << -result << " (" << ::strerror(-result) << ")" << std::endl;
         return result;
     }
 
@@ -303,22 +336,37 @@ int main(int, char**){
         ceph_ll_put(mount.get(), inode);
     });
 
-
-    struct ceph_statx dir_sb_snap_try;
-    result = ceph_ll_getattr(mount.get(), test_dir_inode_snap, &dir_sb_snap_try, CEPH_STATX_ALL_STATS, 0, user_perms.get());
-    if (result < 0) {
-        std::cerr << "Failed to stat snapshot file" << ": error " << -result << " (" << ::strerror(-result) << ")" << std::endl;
+    Inode* test_file_inode_snap = nullptr;
+    result = ceph_ll_lookup_vino(mount.get(), vivo_file, &test_file_inode_snap);
+    if (result) {
+        std::cerr << "Failed to lookup inode of file {" << std::to_string(vivo_file.ino.val) << ", " << std::to_string(vivo_file.snapid.val) << "} in snapshot: error " << -result << " (" << ::strerror(-result) << ")" << std::endl;
         return result;
     }
 
-#if 0
-    const std::string dir_snap_path = snap_path + "/" + dir_name;
-    struct ceph_statx dir_sb_snap;
+    std::shared_ptr<Inode> scoped_test_file_inode_snap(test_file_inode_snap, [mount](Inode* inode) {
+        ceph_ll_put(mount.get(), inode);
+    });
 
-    result = ceph_statx(mount.get(), dir_snap_path.c_str(), &dir_sb_snap, CEPH_STATX_ALL_STATS, 0);
-    if (result) {
-        std::cerr << "Failed to statx directory in snapshot " << dir_snap_path << ": error " << -result << " (" << ::strerror(-result) << ")" << std::endl;
-        return result;
+#if 0
+    {
+      const std::string dir_snap_path = snap_path + "/" + dir_name;
+      struct ceph_statx dir_sb_snap;
+
+      result = ceph_statx(mount.get(), dir_snap_path.c_str(), &dir_sb_snap, CEPH_STATX_ALL_STATS, 0);
+      if (result) {
+          std::cerr << "Failed to statx directory in snapshot " << dir_snap_path << ": error " << -result << " (" << ::strerror(-result) << ")" << std::endl;
+          return result;
+      }
+    }
+    {
+      const std::string file_snap_path = snap_path + "/" + file_name;
+      struct ceph_statx file_sb_snap;
+
+      result = ceph_statx(mount.get(), file_snap_path.c_str(), &file_sb_snap, CEPH_STATX_ALL_STATS, 0);
+      if (result) {
+          std::cerr << "Failed to statx file in snapshot " << file_snap_path << ": error " << -result << " (" << ::strerror(-result) << ")" << std::endl;
+          return result;
+      }
     }
 #elif 0
     struct ceph_statx sb_snap_dir;
@@ -347,19 +395,36 @@ int main(int, char**){
         ceph_ll_put(mount.get(), inode);
     });
 
-    Inode* inode_snap_sub_dir = nullptr;
-    struct ceph_statx sb_snap_sub_dir;
+  {
+    Inode* inode_ = nullptr;
+    struct ceph_statx sb_;
 
-    result = ceph_ll_lookup(mount.get(), inode_snap_path, dir_name.c_str(), &inode_snap_sub_dir, &sb_snap_sub_dir, CEPH_STATX_INO, 0, user_perms.get());
+    result = ceph_ll_lookup(mount.get(), inode_snap_path, dir_name.c_str(), &inode_, &sb_, CEPH_STATX_INO, 0, user_perms.get());
     if (result) {
         std::cerr << "Failed to lookup inode of snapshot sub directory " << dir_name << ": error " << -result << " (" << ::strerror(-result) << ")" << std::endl;
         return result;
     }
 
-    std::shared_ptr<Inode> scoped_inode_snap_sub_dir(inode_snap_sub_dir, [mount](Inode* inode) {
+    std::shared_ptr<Inode> scoped_inode_(inode_, [mount](Inode* inode) {
         ceph_ll_put(mount.get(), inode);
     });
-#else
+  }
+
+  {
+    Inode* inode_ = nullptr;
+    struct ceph_statx sb_;
+
+    result = ceph_ll_lookup(mount.get(), inode_snap_path, file_name.c_str(), &inode_, &sb_, CEPH_STATX_INO, 0, user_perms.get());
+    if (result) {
+        std::cerr << "Failed to lookup inode of snapshot file " << dir_name << ": error " << -result << " (" << ::strerror(-result) << ")" << std::endl;
+        return result;
+    }
+
+    std::shared_ptr<Inode> scoped_inode_(inode_, [mount](Inode* inode) {
+        ceph_ll_put(mount.get(), inode);
+    });
+  }
+#elif 1
   Inode* inode_snap_dir = nullptr;
   struct ceph_statx sb_snap_dir;
 
@@ -443,105 +508,10 @@ int main(int, char**){
 
     std::cout << "snapshot inode " << dir_sb_snap.stx_dev << " >=< " << dir_sb.stx_dev << std::endl;
 
-    if (dir_sb_snap.stx_dev != dir_sb_snap_try.stx_dev) {
-        std::cerr << "Snapshot inode device mismatch: " << dir_sb_snap.stx_dev << " != " << dir_sb_snap_try.stx_dev << std::endl;
-        return -EINVAL;
-    }
-
-    if (dir_sb_snap.stx_ino != dir_sb_snap_try.stx_ino) {
-        std::cerr << "Snapshot inode number mismatch: " << dir_sb_snap.stx_ino << " != " << dir_sb_snap_try.stx_ino << std::endl;
-        return -EINVAL;
-    }
-    
-    if (dir_sb_snap.stx_size != dir_sb_snap_try.stx_size) {
-        std::cerr << "Snapshot inode size mismatch: " << dir_sb_snap.stx_size << " != " << dir_sb_snap_try.stx_size << std::endl;
-        return -EINVAL;
-    }
-
-    if (dir_sb_snap.stx_blocks != dir_sb_snap_try.stx_blocks) {
-        std::cerr << "Snapshot inode blocks mismatch: " << dir_sb_snap.stx_blocks << " != " << dir_sb_snap_try.stx_blocks << std::endl;
-        return -EINVAL;
-    }
-
-    if (dir_sb_snap.stx_mode != dir_sb_snap_try.stx_mode) {
-        std::cerr << "Snapshot inode mode mismatch: " << dir_sb_snap.stx_mode << " != " << dir_sb_snap_try.stx_mode << std::endl;
-        return -EINVAL;
-    }
-
-    if (dir_sb_snap.stx_uid != dir_sb_snap_try.stx_uid) {
-        std::cerr << "Snapshot inode uid mismatch: " << dir_sb_snap.stx_uid << " != " << dir_sb_snap_try.stx_uid << std::endl;
-        return -EINVAL;
-    }
-
-    if (dir_sb_snap.stx_gid != dir_sb_snap_try.stx_gid) {
-        std::cerr << "Snapshot inode gid mismatch: " << dir_sb_snap.stx_gid << " != " << dir_sb_snap_try.stx_gid << std::endl;
-        return -EINVAL;
-    }
-
-    if (dir_sb_snap.stx_nlink != dir_sb_snap_try.stx_nlink) {
-        std::cerr << "Snapshot inode nlink mismatch: " << dir_sb_snap.stx_nlink << " != " << dir_sb_snap_try.stx_nlink << std::endl;
-        return -EINVAL;
-    }
-
-    if (dir_sb_snap.stx_rdev != dir_sb_snap_try.stx_rdev) {
-        std::cerr << "Snapshot inode rdev mismatch: " << dir_sb_snap.stx_rdev << " != " << dir_sb_snap_try.stx_rdev << std::endl;
-        return -EINVAL;
-    }
-
-    if (dir_sb_snap.stx_blksize != dir_sb_snap_try.stx_blksize) {
-        std::cerr << "Snapshot inode blksize mismatch: " << dir_sb_snap.stx_blksize << " != " << dir_sb_snap_try.stx_blksize << std::endl;
-        return -EINVAL;
-    }
-
-    if (dir_sb_snap.stx_version != dir_sb_snap_try.stx_version) {
-        std::cerr << "Snapshot inode version mismatch: " << dir_sb_snap.stx_version << " != " << dir_sb_snap_try.stx_version << std::endl;
-        return -EINVAL;
-    }
-
-    if (dir_sb_snap.stx_atime.tv_sec != dir_sb_snap_try.stx_atime.tv_sec) {
-        std::cerr << "Snapshot inode atime sec mismatch: " << dir_sb_snap.stx_atime.tv_sec << " != " << dir_sb_snap_try.stx_atime.tv_sec << std::endl;
-        return -EINVAL;
-    }
-
-    if (dir_sb_snap.stx_atime.tv_nsec != dir_sb_snap_try.stx_atime.tv_nsec) {
-        std::cerr << "Snapshot inode atime nsec mismatch: " << dir_sb_snap.stx_atime.tv_nsec << " != " << dir_sb_snap_try.stx_atime.tv_nsec << std::endl;
-        return -EINVAL;
-    }
-
-    if (dir_sb_snap.stx_ctime.tv_sec != dir_sb_snap_try.stx_ctime.tv_sec) {
-        std::cerr << "Snapshot inode ctime sec mismatch: " << dir_sb_snap.stx_ctime.tv_sec << " != " << dir_sb_snap_try.stx_ctime.tv_sec << std::endl;
-        return -EINVAL;
-    }
-
-    if (dir_sb_snap.stx_ctime.tv_nsec != dir_sb_snap_try.stx_ctime.tv_nsec) {
-        std::cerr << "Snapshot inode ctime nsec mismatch: " << dir_sb_snap.stx_ctime.tv_nsec << " != " << dir_sb_snap_try.stx_ctime.tv_nsec << std::endl;
-        return -EINVAL;
-    }
-
-    if (dir_sb_snap.stx_mtime.tv_sec != dir_sb_snap_try.stx_mtime.tv_sec) {
-        std::cerr << "Snapshot inode mtime sec mismatch: " << dir_sb_snap.stx_mtime.tv_sec << " != " << dir_sb_snap_try.stx_mtime.tv_sec << std::endl;
-        return -EINVAL;
-    }
-
-    if (dir_sb_snap.stx_mtime.tv_nsec != dir_sb_snap_try.stx_mtime.tv_nsec) {
-        std::cerr << "Snapshot inode mtime nsec mismatch: " << dir_sb_snap.stx_mtime.tv_nsec << " != " << dir_sb_snap_try.stx_mtime.tv_nsec << std::endl;
-        return -EINVAL;
-    }
-
-    if (dir_sb_snap.stx_btime.tv_sec != dir_sb_snap_try.stx_btime.tv_sec) {
-        std::cerr << "Snapshot inode btime sec mismatch: " << dir_sb_snap.stx_btime.tv_sec << " != " << dir_sb_snap_try.stx_btime.tv_sec << std::endl;
-        return -EINVAL;
-    }
-
-    if (dir_sb_snap.stx_btime.tv_nsec != dir_sb_snap_try.stx_btime.tv_nsec) {
-        std::cerr << "Snapshot inode btime nsec mismatch: " << dir_sb_snap.stx_btime.tv_nsec << " != " << dir_sb_snap_try.stx_btime.tv_nsec << std::endl;
-        return -EINVAL;
-    }
-
     //const char* new_xattr_name = "ceph.snap.btime";
     const char* new_xattr_name = xattr_name.c_str();
 
-    result = ceph_ll_getxattr(mount.get(), test_dir_inode_snap, new_xattr_name, nullptr, 0, user_perms.get());
+    result = ceph_ll_getxattr(mount.get(), test_file_inode_snap, new_xattr_name, nullptr, 0, user_perms.get());
     if (result < 0) {
         std::cerr << "Failed to get length of snapshot file's xattr " << new_xattr_name << ": error " << -result << " (" << ::strerror(-result) << ")" << std::endl;
         return result;
@@ -549,9 +519,25 @@ int main(int, char**){
 
     std::string xattr_read(result + 1, '\0');
 
-    result = ceph_ll_getxattr(mount.get(), test_dir_inode_snap, new_xattr_name, xattr_read.data(), result, user_perms.get());
+    result = ceph_ll_getxattr(mount.get(), test_file_inode_snap, new_xattr_name, xattr_read.data(), result, user_perms.get());
     if (result < 0) {
         std::cerr << "Failed to get snapshot file's xattr " << new_xattr_name << ": error " << -result << " (" << ::strerror(-result) << ")" << std::endl;
+        return result;
+    }
+
+    std::cerr << "xattr of file in snapshot: " << snap_id <<  " is: "<< xattr_read << std::endl;
+
+    result = ceph_ll_getxattr(mount.get(), test_dir_inode_snap, new_xattr_name, nullptr, 0, user_perms.get());
+    if (result < 0) {
+        std::cerr << "Failed to get length of snapshot dir's xattr " << new_xattr_name << ": error " << -result << " (" << ::strerror(-result) << ")" << std::endl;
+        return result;
+    }
+
+    xattr_read.resize(result + 1, '\0');
+
+    result = ceph_ll_getxattr(mount.get(), test_dir_inode_snap, new_xattr_name, xattr_read.data(), result, user_perms.get());
+    if (result < 0) {
+        std::cerr << "Failed to get snapshot dir's xattr " << new_xattr_name << ": error " << -result << " (" << ::strerror(-result) << ")" << std::endl;
         return result;
     }
 
